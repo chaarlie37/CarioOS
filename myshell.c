d80a872f1bbb6a34935df60131eb38019fff2ac2
Mario3108 <mrmike3108@gmail.com>
2019-11-07 16:46:02 +0100

    comienzo de practica

--- myshell.c
+++ myshell.c
@@ -1,22 +1,40 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <signal.h>
 #include "parser.h"
 
-int
-main(void) {
-	char buf[1024];
-	tline * line;
-	int i,j;
+char buf[1024];
+tline * line;
+int i,j;
+int pid;
 
-	printf("==> ");	
+void manejador_hijo(int sig){//Aqui se ejecutan los comandos
+
+  if(execvp(line->commands[0].argv[0], line->commands[0].argv)< 0){
+    printf("No se ha encontrado el mandato\n");
+  }
+  exit(0);
+}
+
+int main(void) {
+
+  signal(SIGUSR1, manejador_hijo);
+	printf("msh> ");
 	while (fgets(buf, 1024, stdin)) {
-		
+
 		line = tokenize(buf);
 		if (line==NULL) {
 			continue;
 		}
 		if (line->redirect_input != NULL) {
-			printf("redirección de entrada: %s\n", line->redirect_input);
+			//printf("redirección de entrada: %s\n", line->redirect_input);
+			
+			pid = fork();
+			if(pid == 0){
+
+			}else{
+
+			}
 		}
 		if (line->redirect_output != NULL) {
 			printf("redirección de salida: %s\n", line->redirect_output);
@@ -26,14 +44,23 @@ main(void) {
 		}
 		if (line->background) {
 			printf("comando a ejecutarse en background\n");
-		} 
+		}
 		for (i=0; i<line->ncommands; i++) {
 			printf("orden %d (%s):\n", i, line->commands[i].filename);
 			for (j=0; j<line->commands[i].argc; j++) {
 				printf("  argumento %d: %s\n", j, line->commands[i].argv[j]);
 			}
 		}
-		printf("==> ");	
+
+    pid = fork();
+    if(pid == 0){
+      while(1);
+    }else{
+      kill(pid, SIGUSR1);
+      wait(NULL);
+    }
+
+		printf("msh> ");
 	}
 	return 0;
 }
