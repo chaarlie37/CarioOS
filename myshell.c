2f76468070603d9281088d4809f05a5304f36d70
chaarlie37 <carlos37sm@gmail.com>
2019-12-01 13:31:06 +0100

    Minishell terminada

--- myshell.c
+++ myshell.c
@@ -6,14 +6,10 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <signal.h>
-#include "parser.h"
 #include <sys/types.h>
 #include <sys/wait.h>
+#include "parser.h"
 
-
-//TODO:
-// -estructura de procesos para el jobs
-// -Revisar mensajes y casos de error
 
 int pidFg;
 void manejador(int sig);
@@ -26,11 +22,11 @@ void imprimirPrompt(){
 
 int main(void) {
 
-
-
 	char buf[1024];
 	char buf2[1024];
+	char buff[1024];
 	char buffJobs[2048];
+	char auxbuf[2048];
 	tline * line;
 	int i;
 	int pid;
@@ -46,8 +42,8 @@ int main(void) {
 	char *salida;
 	char salidaCd[1024];
 	char salidaJobs[1024];
-	char buff[1024];
 	int error_bg;
+	int procesoBg;
 	int a;
 	int b;
 	int c;
@@ -60,15 +56,12 @@ int main(void) {
 		int * pids;
     } tProcesoBackground;
 
-    //tProcesoBackground procesosBackground[10]; // hay que hacerlo dinamico tb
 	tProcesoBackground **procesosBackground;
 	tProcesoBackground *procesoBackground;
     int contadorProcesosBackground = 0;
 
 	float n = 100000;
 	printf("\033[1;36m");
-
-
 	printf("\n\n\n     ██████╗ █████╗ ██████╗ ██╗ ██████╗      ██████╗  ██████╗\n");
 	usleep(n);
 	printf("    ██╔════╝██╔══██╗██╔══██╗██║██╔═══██╗    ██╔═══██╗██╔════╝\n");
@@ -81,19 +74,16 @@ int main(void) {
 	usleep(n);
 	printf("     ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝ ╚═════╝      ╚═════╝ ╚═════╝ \n\n\n");
 	usleep(n);
-
 	printf("\033[0m");
 	printf("\033[1m");
-	//printf("\033[1;37m");
-
 	printf("Práctica MINISHELL para Sistemas Operativos. Universidad Rey Juan Carlos.\n");
 	printf("Hecho por Carlos Sánchez Muñoz y Mario Manzaneque Ruiz. Noviembre de 2019.\n\n\n");
-
 	printf("\033[0m");
 
 	signal(SIGINT, SIG_IGN);
 	signal(SIGQUIT, SIG_IGN);
 	imprimirPrompt();
+
 	while (fgets(buf, 1024, stdin)) {
 
         line = tokenize(buf);
@@ -111,34 +101,30 @@ int main(void) {
 		// comprobación con waitpid(WNOHANG) para comprobar los hijos que hayan muerto en bg y que no se queden zombies,
 		// además de comprobación de errores al haber ejecutado mandatos en bg
 		for( a = 0; a<contadorProcesosBackground; a++){
+
 			if(procesosBackground[a]->status == 0){
 				b = 0;
 				error_bg = 0;
 				while(b<procesosBackground[a]->n_mandatos && error_bg == 0){
-					//printf("Proceso %d\n", procesosBackground[a]->pids[b]);
 					if(waitpid(procesosBackground[a]->pids[b], &status, WNOHANG) != 0){
-						//printf("A\n");
 						if(WIFEXITED(status) != 0){
-					//		printf("B\n");
+
 							if(WEXITSTATUS(status) != 0){	// para mandatos que salgan con error o no existan
-					//			printf("C\n");
 								error_bg = 1;	// true
 								procesosBackground[a]->status = -1;
 								for( c = a; c<contadorProcesosBackground-1; c++){
 									procesosBackground[c] = procesosBackground[c+1];
 								}
-								free(procesosBackground[contadorProcesosBackground]);
+								free(procesosBackground[contadorProcesosBackground-1]);
 								if(contadorProcesosBackground>0)
 									contadorProcesosBackground--;
 							}else{
 								if(b == procesosBackground[a]->n_mandatos - 1){
+									// si es el ultimo en la mayoria de casos significa que han terminado los demás
 									procesosBackground[a]->status = 1;
-									printf("[%d]+   Done       %s", procesosBackground[a]->n, procesosBackground[a]->nombre);
-									procesosBackground[a]->status = -1;
 								}
 							}
 						}else{	// para mandatos que no han sido encontrados
-							//printf("D\n");
 							error_bg = 1;	// true
 							procesosBackground[a]->status = -1;
 							for( c = a; c<contadorProcesosBackground-1; c++){
@@ -151,9 +137,17 @@ int main(void) {
 					}
 					b++;
 				}
-				for( c = 0; c<procesosBackground[a]->n_mandatos; c++){
-					waitpid(procesosBackground[a]->pids[c], NULL, WNOHANG);
+				// para el caso en el que el ultimo ha terminado, si alguno del medio no ha terminado poner el status a 0
+				if(contadorProcesosBackground > 0){
+					for( c = 0; c<procesosBackground[a]->n_mandatos; c++){
+						if(waitpid(procesosBackground[a]->pids[c], NULL, WNOHANG) == 0){
+							procesosBackground[a]->status = 0;
+						}
+					}
 				}
+			}else if(procesosBackground[a]->status == 1){
+				printf("[%d]+   Done       %s", procesosBackground[a]->n, procesosBackground[a]->nombre);
+				procesosBackground[a]->status = -1;
 			}
 		}
 
@@ -168,9 +162,7 @@ int main(void) {
 					strcat(salidaJobs, "\n");
                     strcpy(buff, salidaJobs);
                     fputs(buff, stderr);
-					//continue;
 				}else{
-					char auxbuf[2048];
 					strcpy(buffJobs, "");
 					for( a = 0; a<contadorProcesosBackground; a++){
 						if(procesosBackground[a]->status == 0){
@@ -198,13 +190,12 @@ int main(void) {
 			signal(SIGQUIT, manejador);
 			if(contadorProcesosBackground > 0){
 				if(!(line->commands[0].argv[1] == NULL)){
-					int procesoBg = atoi(line->commands[0].argv[1]);
-					if(procesoBg > 0 && procesoBg <= contadorProcesosBackground){
+					procesoBg = atoi(line->commands[0].argv[1]);
+					if(procesoBg > 0 && procesoBg <= contadorProcesosBackground && procesosBackground[procesoBg-1]->status == 0){
+						printf("%s\n", procesosBackground[procesoBg-1]->nombre);
 						for( a = 0; a<procesosBackground[procesoBg-1]->n_mandatos; a++){
-							printf("%s\n", procesosBackground[procesoBg-1]->nombre);
 							pidFg = procesosBackground[procesoBg-1]->pids[a];
 							waitpid(procesosBackground[procesoBg-1]->pids[a], NULL, 0);
-
 						}
 						// aqui se pone como status al procesoBG en 1 (Done) ya que le hemos esperado en el wait,
 						// por lo que ha terminado
@@ -213,6 +204,7 @@ int main(void) {
 						printf("myshell: fg: %d: no existe ese trabajo\n", procesoBg);
 					}
 				}else{
+					printf("%s\n", procesosBackground[contadorProcesosBackground-1]->nombre);
 					for( a = 0; a<procesosBackground[contadorProcesosBackground-1]->n_mandatos; a++){
 						pidFg = procesosBackground[contadorProcesosBackground-1]->pids[a];
 						waitpid(procesosBackground[contadorProcesosBackground-1]->pids[a], NULL, 0);
@@ -240,16 +232,14 @@ int main(void) {
 					fscanf(archivo, "%s", buf2);
 					if(chdir(buf2) != 0){//se ejecuta el mandato cd usando el contenido del fichero
 					  fprintf(stderr, "Error: %s\n", strerror(errno));
-					}
+				  	}else{
+					  	printf("Directorio actual: %s\n", buf2);
+				  	}
+
 				}
-
-
 			}else{
-
-
 				if(line->commands[0].argc == 2){//si se le introduce una ruta a la que ir
 					dir = line->commands[0].argv[1];
-
 				}else{
 					//si no se introduce una ruta a la que ir, el mandato hace un cambio de directorio al contenido de la variable HOME
 					dir = getenv("HOME");
@@ -270,12 +260,12 @@ int main(void) {
 							fichero = open(line->redirect_error, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 							dup2(fichero, 2);
 						}
-
 					}
 				  fprintf(stderr, "Error: %s\n", strerror(errno));
-				}
+			  }else{
+				 printf("Directorio actual: %s\n", dir);
+			  }
 			}
-
     	}else if(line->ncommands == 1){			//Caso de que solo haya un mandato
             pipe(fd);	// pipe para redirecciones
             pid = fork();
@@ -356,7 +346,7 @@ int main(void) {
 					procesosBackground = (tProcesoBackground **) malloc(sizeof(tProcesoBackground*));
 				}
 				else{
-					procesoBackground = (tProcesoBackground *) malloc(line->ncommands * sizeof(int) + sizeof(tProcesoBackground));
+					procesoBackground = (tProcesoBackground *) malloc(sizeof(tProcesoBackground));
 					tProcesoBackground **tmp = (tProcesoBackground **) realloc(procesosBackground, (contadorProcesosBackground+1) * sizeof(tProcesoBackground*));
 					procesosBackground = tmp;
 				}
@@ -364,8 +354,6 @@ int main(void) {
                 if(pid < 0){
                     fprintf(stderr, "Error en la creacion del proceso hijo.\n");
                 }else if(pid == 0){
-					//signal(SIGINT, SIG_DFL);
-					//signal(SIGQUIT, SIG_DFL);
                     if(execvp(line->commands[0].argv[0], line->commands[0].argv) < 0){
 						fprintf(stderr, "%s: No se ha encontrado el mandato.\n", line->commands[0].argv[0]);
 						exit(1);
@@ -378,7 +366,7 @@ int main(void) {
 					procesosBackground[contadorProcesosBackground]->status = 0;
 					procesosBackground[contadorProcesosBackground]->n_mandatos = 1;
 					contadorProcesosBackground++;
-                    fprintf(stdin, "[%d] %d\n", contadorProcesosBackground , procesosBackground[contadorProcesosBackground-1]->pids[0]);
+                    printf("[%d] %d\n", contadorProcesosBackground , procesosBackground[contadorProcesosBackground-1]->pids[0]);
 					sleep(1);
                 }
             }else{
@@ -430,7 +418,6 @@ int main(void) {
 							}else{
 								p_h = open(line->redirect_input, O_RDONLY);
 							}
-
                             dup2(p_h, fd[0]);
                             close(fd[0]);
                         }
@@ -541,12 +528,11 @@ int main(void) {
 				signal(SIGQUIT, SIG_IGN);
 				// reserva de memoria para almacenar los procesos en background
 				if(contadorProcesosBackground == 0){
-					procesoBackground = (tProcesoBackground *) malloc(line->ncommands * sizeof(int) + sizeof(tProcesoBackground));
+					procesoBackground = (tProcesoBackground *) malloc(sizeof(tProcesoBackground));
 					procesosBackground = (tProcesoBackground **) malloc(sizeof(tProcesoBackground*));
 				}
 				else{
-					// REVISAR ESTA LINEA
-					procesoBackground = (tProcesoBackground *) malloc(line->ncommands * sizeof(int) + sizeof(tProcesoBackground));
+					procesoBackground = (tProcesoBackground *) malloc(sizeof(tProcesoBackground));
 					tProcesoBackground **tmp = (tProcesoBackground **) realloc(procesosBackground, (contadorProcesosBackground+1) * sizeof(tProcesoBackground*));
 					procesosBackground = tmp;
 				}
@@ -582,10 +568,11 @@ int main(void) {
 				for(a = 0; a<line->ncommands; a++){
 	                waitpid(hijos[a], &status, 0);
 	            }
+
             }
 
 			// se libera en memoria los pipes y los pids de los hijos
-			for(a = 0; a<line->ncommands; a++){
+			for(a = 0; a<line->ncommands-1; a++){
 				free(pipes[a]);
 			}
             free(pipes);
