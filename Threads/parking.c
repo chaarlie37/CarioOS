970dec12f2c8f117db2bfcdc145b1ae9c8a22a72
chaarlie37 <carlos37sm@gmail.com>
2019-12-09 13:04:05 +0100

    Funciona con varias plantas con camiones y coches. En teoria no hay bloq

--- Threads/parking.c
+++ Threads/parking.c
@@ -17,6 +17,10 @@ pthread_mutex_t mutexGen;
 pthread_mutex_t mutex[PLAZAS];
 pthread_cond_t espera[COCHES];
 
+int contador[COCHES];
+
+int coche_id[COCHES + CAMIONES];
+
 void print_estado(){
     printf("Parking: \n");
     for(int i = 0; i<PLANTAS; i++){
@@ -25,32 +29,42 @@ void print_estado(){
         }
         printf("\n");
     }
-    printf("\n");
+    //printf("\n");
+    printf("CONTADORES: \n");
+    for(int i = 0; i<COCHES + CAMIONES; i++){
+        printf("[%d: %d]", coche_id[i], contador[i]);
+    }
+    printf("\n\n");
 }
 
 void *camionAparca(void *num){
     int coche_id = *(int *) num;
     int ocupado = 1;
-    int i;
+    int i,j;
     //sleep(2);
     while(1){
         estado_coches[coche_id] = 0;
         pthread_mutex_lock(&mutexGen);
         if(plazas_libres > 1){
 
-            for(i = 0 ; i<PLAZAS; i++){
-                if(aparcamiento[i] == 0 && aparcamiento [i + 1] == 0){
-                    //pthread_mutex_lock(&mutex[i]);
+            for(i = 0 ; i<PLANTAS; i++){
+                for(j = 1; j<PLAZAS; j++){
+                    if(aparcamiento_plantas[i][j-1] == 0 && aparcamiento_plantas[i][j] == 0){
+                        break;
+                    }
+                }
+                if(aparcamiento_plantas[i][j-1] == 0 && aparcamiento_plantas[i][j] == 0){
                     break;
                 }
             }
 
-            aparcamiento[i] = coche_id;
-            aparcamiento[i+1] = coche_id;
+            aparcamiento_plantas[i][j] = coche_id;
+            aparcamiento_plantas[i][j-1] = coche_id;
             estado_coches[coche_id] = 1;
             plazas_libres -= 2;
+            contador[coche_id % 100]++;
             //pthread_mutex_unlock(&mutex[i]);
-            printf("ENTRADA: Camión %d aparca en %d. Plazas libres: %d.\n", coche_id, i, plazas_libres);
+            printf("ENTRADA: Camión %d aparca en %d y %d de planta %d. Plazas libres: %d.\n", coche_id, j-1, j, i, plazas_libres);
             print_estado();
             pthread_mutex_unlock(&mutexGen);
 
@@ -58,8 +72,8 @@ void *camionAparca(void *num){
 
             pthread_mutex_lock(&mutexGen);
             //pthread_mutex_lock(&mutex[i]);
-            aparcamiento[i] = 0;
-            aparcamiento[i+1] = 0;
+            aparcamiento_plantas[i][j] = 0;
+            aparcamiento_plantas[i][j-1] = 0;
             estado_coches[coche_id] = 0;
             plazas_libres += 2;
             printf("SALIENDO: Camión %d saliendo. Plazas libres: %d.\n", coche_id, plazas_libres);
@@ -94,6 +108,7 @@ void * cocheAparca(void *num){
             aparcamiento_plantas[i][j] = coche_id;
             estado_coches[coche_id] = 1;
             plazas_libres -= 1;
+            contador[coche_id]++;
             //pthread_mutex_unlock(&mutex[i]);
             printf("ENTRADA: coche %d aparca en %d de la planta %d. Plazas libres: %d.\n", coche_id, j, i, plazas_libres);
             print_estado();
@@ -119,7 +134,7 @@ int main(){
     pthread_t coches[COCHES];
     pthread_t camiones[CAMIONES];
     int i, j;
-    int coche_id[COCHES + CAMIONES];
+
     pthread_mutex_init(&mutexGen, NULL);
     for(i = 0; i<PLANTAS; i++){
         for(j = 0; j<PLAZAS; j++){
@@ -136,13 +151,13 @@ int main(){
         pthread_create(&coches[i], NULL, cocheAparca, (void *) &coche_id[i]);
     }
 
-/*
+
     for(j = i; j-i<CAMIONES; j++){
         //pthread_cond_init(&espera,  NULL);
         coche_id[j] = j+1 + 100;
         pthread_create(&camiones[j], NULL, camionAparca, (void *) &coche_id[j]);
     }
-*/
+
 
     while(1);
     //comentario
